e6ec8cf29e3fcb65107bd94b81018e6300b2f1ea
caro.ortiz9501@gmail.com <carolinaortiz204424@correo.itm.edu.co>
2026-03-09 23:46:15 -0500

    Creación del código con validación de credenciales usando ciclo do while

---
+++ login.js
@@ -0,0 +1,30 @@
+const usuarioGuardado = "Scath";
+const contrasenaGuardada = "Pepadeaguacate";
+let cantidadIntentoss = 3;
+
+do{
+    let usuarioIngresado = prompt(`Por favor ingrese su usuario: `)
+    if (usuarioIngresado === usuarioGuardado) {
+        let contrasenaIngresada = prompt(`Por favor ingrese su contraseña: `)
+        if (contrasenaIngresada === contrasenaGuardada) {
+            break;
+        }else{
+            cantidadIntentoss--;
+            console.log(`La contraseña ingresada es incorrecta, por favor intente nuevamente, y recuerde que tiene ${cantidadIntentoss} intentos`);
+        }
+    }else{
+        cantidadIntentoss--;
+        if (cantidadIntentoss > 1){
+            console.log(`El usuario ingresado es incorrecto, por favor intente nuevamente, y recuerde que tiene ${cantidadIntentoss} intentos`);
+        }else if(cantidadIntentoss === 1){
+            console.log(`El usuario ingresado es incorrecto, por favor intente nuevamente, y recuerde que tiene ${cantidadIntentoss} intento`);
+        }
+        else{
+            console.log(`El usuario ha sido bloqueado por agotar la cantidad de intentos`);
+        }
+    }
+}while(cantidadIntentoss > 0);
+
+if (cantidadIntentoss != 0) {
+    console.log(`Bienvenido al sistema 😊`);
+}
