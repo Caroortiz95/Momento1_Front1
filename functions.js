b12fa53bf497146b3bf9ba47c39982122f4d35cd
caro.ortiz9501@gmail.com <carolinaortiz204424@correo.itm.edu.co>
2026-03-11 13:43:24 -0500

    Se encapsula la validación completa de los datos

--- functions.js
+++ functions.js
@@ -16,4 +16,39 @@ function validacionContrasena(contrasenaGuardada, contrasenaIngresada){
     return esCorrecto;
 }
 
-export {validacionNombre, validacionContrasena};
+function mensajeIntentos(cantidadIntentos){
+    if (cantidadIntentos > 1){
+            console.log(`El usuario ingresado es incorrecto, por favor intente nuevamente, y recuerde que tiene ${cantidadIntentos} intentos`);
+        }else if(cantidadIntentos === 1){
+            console.log(`El usuario ingresado es incorrecto, por favor intente nuevamente, y recuerde que tiene ${cantidadIntentos} intento`);
+        }
+        else{
+            console.log(`El usuario ha sido bloqueado por agotar la cantidad de intentos`);
+        }
+}
+
+function validacionCredenciales(usuarioGuardado, contrasenaGuardada){
+    let cantidadIntentos = 3;
+    do{
+    let usuarioIngresado = prompt(`Por favor ingrese su usuario: `)
+    if (validacionNombre(usuarioGuardado, usuarioIngresado) === true) {
+        do{
+            let contrasenaIngresada = prompt(`Por favor ingrese su contraseña: `)
+        if (validacionContrasena(contrasenaGuardada, contrasenaIngresada) === true) {
+            break;
+        }else{
+            cantidadIntentos--;
+            mensajeIntentos(cantidadIntentos);
+        }
+        }while(cantidadIntentos > 0);
+        break;
+    }else{
+        cantidadIntentos--;
+        mensajeIntentos(cantidadIntentos);
+    }
+}while(cantidadIntentos > 0);
+
+return cantidadIntentos;
+}
+
+export {validacionCredenciales};
--- login.js
+++ login.js
@@ -1,33 +1,10 @@
-import { validacionNombre, validacionContrasena } from "./functions.js";
+import {validacionCredenciales } from "./functions.js";
 
 const usuarioGuardado = "Scath";
 const contrasenaGuardada = "Pepadeaguacate";
-let cantidadIntentoss = 3;
 
-do{
-    let usuarioIngresado = prompt(`Por favor ingrese su usuario: `)
-    if (validacionNombre(usuarioGuardado, usuarioIngresado) === true) {
-        let contrasenaIngresada = prompt(`Por favor ingrese su contraseña: `)
-        if (validacionContrasena(contrasenaGuardada, contrasenaIngresada) === true) {
-            break;
-        }else{
-            cantidadIntentoss--;
-            console.log(`La contraseña ingresada es incorrecta, por favor intente nuevamente, y recuerde que tiene ${cantidadIntentoss} intentos`);
-        }
-    }else{
-        cantidadIntentoss--;
-        if (cantidadIntentoss > 1){
-            console.log(`El usuario ingresado es incorrecto, por favor intente nuevamente, y recuerde que tiene ${cantidadIntentoss} intentos`);
-        }else if(cantidadIntentoss === 1){
-            console.log(`El usuario ingresado es incorrecto, por favor intente nuevamente, y recuerde que tiene ${cantidadIntentoss} intento`);
-        }
-        else{
-            console.log(`El usuario ha sido bloqueado por agotar la cantidad de intentos`);
-        }
-        
-    }
-}while(cantidadIntentoss > 0);
+const cantidadIntentos = validacionCredenciales(usuarioGuardado, contrasenaGuardada);
 
-if (cantidadIntentoss != 0) {
+if (cantidadIntentos != 0) {
     console.log(`Bienvenido al sistema 😊`);
 }
